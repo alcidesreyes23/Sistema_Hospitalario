f582e40c6684f4442e1551bbd72bb27bc0ac4b37
alcidesreyes98 <erick.reyes2398@gmail.com>
2022-06-25 21:38:27 -0600

    wip: routing and modules added

---
+++ src/app/pages/page-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PageRoutingRoutingModule } from './page-routing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PageRoutingRoutingModule
+  ]
+})
+export class PageRoutingModule { }
