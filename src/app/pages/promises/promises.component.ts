af7bba75c1fd3cea7b2c3cef38390bbc35ea9377
ereyes <ereyes@cikume.com>
2022-08-21 20:26:36 -0600

    component methodS

--- src/app/pages/promises/promises.component.ts
+++ src/app/pages/promises/promises.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+const GOOD_NUMBER = '(333) 969-6999';
+const BAD_NUMBER = '+(333)969-6999';
 
 @Component({
   selector: 'app-promises',
@@ -7,13 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromisesComponent implements OnInit {
 
-  constructor() { }
+  userForm!: FormGroup;
+  phoneNumberVerifying:boolean = false;
+  constructor(private fb:FormBuilder) { 
+    this.createForm();
+  }
 
   ngOnInit(): void {
 
    this.getUsers().then((res) => {
     console.log('HEY AQUI ESTAN LOS DATOS: ',res);
    });
+  }
+
+  createForm(){
+    //const phoneValidation = 
+    this.userForm = this.fb.group({
+       phone:[this.phoneNumberValidation(BAD_NUMBER) ? GOOD_NUMBER: BAD_NUMBER,Validators.required],
+    });
   }
 
   getUsers(): Promise<any> {
@@ -23,6 +39,42 @@ export class PromisesComponent implements OnInit {
          res.json().then(body => resolve(body.data))
       })
     })
+  }
+
+  phoneNumberValidation(phone:string): boolean{
+    const expresion = /^[(]?[0-9]{3}[)][\s][0-9]{3}[-][0-9]{4}$/;
+    // let bad = ' +255-966-9984 ';
+    // let good ='(255) 255-9665';
+    const result = phone.match(expresion);
+    if(result)
+    {
+      console.log('EXCELENTE :)')
+      console.log('Resultado: ', result);
+      this.phoneNumberVerifying = true;
+      return this.phoneNumberVerifying;
+    }else{
+      console.log('MALA SUERTE :(')
+      console.log('Resultado: ', result);
+      this.phoneNumberVerifying = false;
+      return this.phoneNumberVerifying;
+    }
+  }
+
+  send():void {
+    const expresion = /^[(]?[0-9]{3}[)][\s][0-9]{3}[-][0-9]{4}$/;
+    let bad = ' +255-966-9984 ';
+    let good ='(255) 255-9665';
+    const result = good.match(expresion);
+    if(result)
+    {
+      console.log('EXCELENTE :)')
+      console.log('Resultado: ', result);
+    }else{
+      console.log('MALA SUERTE :(')
+      console.log('Resultado: ', result);
+    }
+    
+    console.log('DATOS: ',this.userForm.getRawValue());
   }
  
 
